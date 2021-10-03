c068e7278b5c96dc5a441d35090718fe020abe3d
korimse <korimse@gmail.com>
2021-10-03 21:04:49 +0900

    feat: 기본 설정

---
+++ src/main/java/com/cloneCoin/push/controller/MainController.java
@@ -0,0 +1,29 @@
+package com.cloneCoin.push.controller;
+
+import com.cloneCoin.push.domain.Message;
+import com.cloneCoin.push.dto.InfoDto;
+import com.cloneCoin.push.dto.SendMessageDto;
+import com.cloneCoin.push.service.MessageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import javax.ws.rs.Path;
+import java.util.List;
+
+@RestController
+@RequestMapping("/push")
+@RequiredArgsConstructor
+public class MainController {
+
+    private final MessageService messageService;
+
+    @GetMapping("/welcome")
+    public String Main(){
+        return "Hello Push Server";
+    }
+
+    @PostMapping("/previous")
+    public List<SendMessageDto> getPrevious(@RequestBody InfoDto infoDto){
+        return messageService.getPrevious(infoDto);
+    }
+}
