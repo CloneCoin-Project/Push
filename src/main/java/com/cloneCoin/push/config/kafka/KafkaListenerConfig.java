f977c4208a2d54dad6476883f3b6eedb3b9ac746
korimse <korimse@gmail.com>
2021-10-03 21:05:05 +0900

    feat: kafka & websocket 설정

---
+++ src/main/java/com/cloneCoin/push/config/kafka/KafkaListenerConfig.java
@@ -0,0 +1,59 @@
+package com.cloneCoin.push.config.kafka;
+
+import com.cloneCoin.push.domain.Message;
+import com.cloneCoin.push.dto.ReceiveDto;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableKafka
+@Slf4j
+public class KafkaListenerConfig {
+    @Bean
+    ConcurrentKafkaListenerContainerFactory<String, ReceiveDto> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, ReceiveDto> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        factory.setErrorHandler(((e, consumerRecord) ->{
+            log.error("error : "+ e);
+            log.info("잘못된 형식이 들어왔습니다.");
+        }));
+
+        return factory;
+    }
+
+    @Bean
+    public ConsumerFactory<String, ReceiveDto> consumerFactory() {
+        return new DefaultKafkaConsumerFactory<>(consumerConfigurations(), new StringDeserializer(),
+                new ErrorHandlingDeserializer(new JsonDeserializer<>(ReceiveDto.class)));
+
+    }
+
+    @Bean
+    public Map<String, Object> consumerConfigurations() {
+        JsonDeserializer<Object> deserializer = new JsonDeserializer<>(Object.class);
+        deserializer.setRemoveTypeHeaders(false);
+        deserializer.addTrustedPackages("*");
+        deserializer.setUseTypeMapperForKey(true);
+
+        Map<String, Object> configurations = new HashMap<>();
+        configurations.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, KafkaConstants.KAFKA_BROKER);
+        configurations.put(ConsumerConfig.GROUP_ID_CONFIG, KafkaConstants.GROUP_ID);
+        configurations.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        configurations.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, deserializer);
+        configurations.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        return configurations;
+    }
+}
---
+++ src/main/java/com/cloneCoin/push/config/kafka/KafkaProducerConfig.java
@@ -0,0 +1,36 @@
+package com.cloneCoin.push.config.kafka;
+
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@EnableKafka
+@Configuration
+public class KafkaProducerConfig {
+    @Bean
+    public ProducerFactory<String, Object> producerFactory() {
+        return new DefaultKafkaProducerFactory<>(ProducerConfigurations());
+    }
+
+    @Bean
+    public Map<String, Object> ProducerConfigurations() {
+        Map<String, Object> configurations = new HashMap<>();
+        configurations.put(org.apache.kafka.clients.producer.ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, KafkaConstants.KAFKA_BROKER);
+        configurations.put(org.apache.kafka.clients.producer.ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configurations.put(org.apache.kafka.clients.producer.ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return configurations;
+    }
+
+    @Bean
+    public KafkaTemplate<String, Object> kafkaTemplate(){
+        return new KafkaTemplate<>(producerFactory());
+    }
+}
