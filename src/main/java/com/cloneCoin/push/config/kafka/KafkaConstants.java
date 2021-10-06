373317379c292e80b39ecc89e525d11d7b1ca88f
korimse <korimse@gmail.com>
2021-10-06 20:13:50 +0900

    refactor: kafka 서버

--- src/main/java/com/cloneCoin/push/config/kafka/KafkaConstants.java
+++ src/main/java/com/cloneCoin/push/config/kafka/KafkaConstants.java
@@ -2,5 +2,5 @@ package com.cloneCoin.push.config.kafka;
 
 public class KafkaConstants {
     public static final String GROUP_ID = "foo";
-    public static final String KAFKA_BROKER = "localhost:9092";
+    public static final String KAFKA_BROKER = "13.125.171.36:9092";
 }
